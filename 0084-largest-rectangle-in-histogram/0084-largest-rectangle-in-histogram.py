f66fa714afd284d2a8b950a10570b280dab03a01
Abhinowever <abhinowever@gmail.com>
2026-08-22 22:22:42 +0530

    Time: 202 ms (79.66%), Space: 20.7 MB (88.15%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -7,13 +7,17 @@ class Solution(object):
         stack = []
         maxx = 0
         
-        heights.append(0)
+        heights.append(0) # adding this will results in traversion of all the elements.
 
         for i ,h in enumerate(heights) :
-            while stack and heights[stack[-1]] > h :
-                height = heights[stack.pop()]
+
+            while stack and heights[stack[-1]] > h : # weather the current bar is shorter than the last bigger bar
+
+                height = heights[stack.pop()] # calculating the maximum area could be get by the recent largest bar
+
+
                 if stack :
-                    width = i - stack[-1] - 1
+                    width = i - stack[-1] - 1 # i -> next smaller, stack[-1] -> previous smaller 
                 else :
                     width = i
                 
